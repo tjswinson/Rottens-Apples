3ab942e8a8c8f60e7280dc1244d0fe61d757e2ca
tjswinson <90232436+tjswinson@users.noreply.github.com>
2021-12-04 11:53:44 -0500

    Delete Groovy.js

--- Groovy.js
+++
@@ -1,4 +0,0 @@
-onst getMovies = () => {
-    fetch(‘https://imdb-api.com/en/API/Keyword/k_i4gqi304/fight’).then(response => response.json()).then(data => console.log(data));
-    }
-    getMovies()
